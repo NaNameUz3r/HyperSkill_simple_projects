c1e6f6e53b6100d5b811f36f8b5edd60b50e34d6
NaNameUz3r <f4ckj00gl3@gmail.com>
2021-04-23 23:57:23 +0300

    update AI in domino, add cool banner! yup!

--- dominoes/dominoes.py
+++ dominoes/dominoes.py
@@ -1,6 +1,31 @@
 import random
 
 
+def rules():
+
+    print("""
+    
+██████╗  ██████╗ ███╗   ███╗██╗███╗   ██╗ ██████╗ 
+██╔══██╗██╔═══██╗████╗ ████║██║████╗  ██║██╔═══██╗
+██║  ██║██║   ██║██╔████╔██║██║██╔██╗ ██║██║   ██║
+██║  ██║██║   ██║██║╚██╔╝██║██║██║╚██╗██║██║   ██║
+██████╔╝╚██████╔╝██║ ╚═╝ ██║██║██║ ╚████║╚██████╔╝
+╚═════╝  ╚═════╝ ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝       
+
+Hey! It's a simple domino game against your computer!
+When it's your turn, make your choice by entering
+the number of piece that you have in your stock.
+
+If you want to put this piece on the right side
+domino snake - enter a positive number!
+If on the left - negative!
+
+Good luck buddy! 
+                     
+    """)
+    _ = input('Press Enter to start game!')
+
+
 def message(stock, computer, snake_pieces, player):
     if len(snake_pieces) > 6:
         print('=' * 70,
@@ -18,7 +43,7 @@ def message(stock, computer, snake_pieces, player):
         print('{}:{}'.format(piece + 1, player[piece]))
 
 
-def winner(computer_check, snake_check, player_check):
+def winner(computer_check, snake_check, player_check, stock):
     if len(player_check) == 0:
         print('\nStatus: The game is over. You won!')
         return True
@@ -28,12 +53,19 @@ def winner(computer_check, snake_check, player_check):
     if snake_check[0][0] == snake_check[-1][1] and sum(x.count(snake_check[0][0]) for x in snake_check) == 8:
         print('\nStatus: The game is over. It\'s a draw!')
         return True
+    if stock == [] and pass_counter >= 2:
+        print('\nStatus: The game is over. It\'s a draw!')
+        return True
+
+
+rules()
 
 
 while True:
     stock_pieces = [[x, y] for x in range(0, 7) for y in range(x, 7)]
     computer_pieces, player_pieces, snake = [list() for _ in range(0, 3)]
     next_move = ''
+    pass_counter = 0
 
     for _ in range(0, 7):
         n = random.randrange(0, len(stock_pieces), 1)
@@ -60,11 +92,12 @@ while True:
     if next_move:
         break
 
+
 message(stock_pieces, computer_pieces, snake, player_pieces)
 
 while True:
 
-    if winner(computer_pieces, snake, player_pieces):
+    if winner(computer_pieces, snake, player_pieces, stock_pieces):
         break
 
     if next_move == 'player':
@@ -77,6 +110,7 @@ while True:
 
                 if int(move) == 0 and stock_pieces == []:
                     next_move = 'computer'
+                    pass_counter += 1
                     break
 
                 elif int(move) == 0 and stock_pieces != []:
@@ -117,21 +151,44 @@ while True:
                 move = input('Invalid input. Please try again.\n')
 
     else:
-        move = input("\nStatus: Computer is about to make a move. Press Enter to continue...\n")
+        foo = input("\nStatus: Computer is about to make a move. Press Enter to continue...\n")
 
-        while True:
+        sum_list = computer_pieces + snake
+        joined_pieces = []
+        for i in sum_list:
+            for j in i:
+                joined_pieces.append(j)
 
-            move = random.randrange(-len(computer_pieces), len(computer_pieces), 1)
+        analysis_mask = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
 
-            if move == 0 and stock_pieces != []:
+        for i in joined_pieces:
+            analysis_mask[i] += 1
+
+        domino_values = {}
+
+        for i in range(len(computer_pieces)):
+            domino_values[computer_pieces.index(computer_pieces[i])] = (
+                    analysis_mask[computer_pieces[i][0]] + analysis_mask[computer_pieces[i][1]])
+
+        domino_values = sorted(domino_values.items(), key=lambda x: x[1], reverse=True)
+        domino_values = dict(domino_values)
+        play_pieces_indexes = [*domino_values]
+
+        # while True:
+        for i in play_pieces_indexes:
+
+            move = play_pieces_indexes[i] + 1
+
+            if move == len(play_pieces_indexes) and stock_pieces != []:
                 n = random.randrange(0, len(stock_pieces), 1)
                 computer_pieces.append(stock_pieces[n])
                 stock_pieces.remove(stock_pieces[n])
                 next_move = 'player'
                 break
 
-            elif move == 0 and stock_pieces == []:
+            elif move == len(play_pieces_indexes) and stock_pieces == []:
                 next_move = 'player'
+                pass_counter += 1
                 break
 
             elif move > 0 and snake[-1][1] == computer_pieces[move - 1][0]:
