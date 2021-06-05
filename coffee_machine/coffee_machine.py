e256705abbf01d0b0cb38e1abb073c48dbd7703b
NaNameUz3r <f4ckj00gl3@gmail.com>
2021-06-05 16:40:29 +0300

    refactored names

--- coffee_machine/coffee_machine.py
+++ coffee_machine/coffee_machine.py
@@ -1,4 +1,4 @@
-remainings = {'water': 400,
+ingredients_stock = {'water': 400,
               'milk': 540,
               'beans': 120,
               'cups': 9,
@@ -9,11 +9,11 @@ def main():
     while True:
         action = str(input("Write action (buy, fill, take, remaining, exit):"))
         if action == "buy":
-            buy()
+            process_user_choice()
         elif action == "fill":
-            fill()
+            replenish_stock()
         elif action == "take":
-            take()
+            extract_money()
         elif action == "remaining":
             status_printer()
         elif action == 'exit':
@@ -23,92 +23,92 @@ def main():
             main()
 
 
-def status_printer():
+def show_stock_values():
 
     print("The coffee machine has:")
-    print(str(remainings['water']), " of water")
-    print(str(remainings['milk']), " of milk")
-    print(str(remainings['beans']), " of coffee beans")
-    print(str(remainings['cups']), " of disposable cups")
-    print(str(remainings['money']), " of money")
+    print(str(ingredients_stock['water']), " of water")
+    print(str(ingredients_stock['milk']), " of milk")
+    print(str(ingredients_stock['beans']), " of coffee beans")
+    print(str(ingredients_stock['cups']), " of disposable cups")
+    print(str(ingredients_stock['money']), " of money")
 
 
-def buy():
+def process_user_choice():
     cup_choice = input("What do you want to buy? 1 - espresso, 2 - latte, "
                        "3 - cappuccino, back - to main menu:")
     if cup_choice == 'back':
         main()
     else:
-        coffee_maker(int(cup_choice))
+        make_coffee(int(cup_choice))
 
 
-def fill():
+def replenish_stock():
 
     add_water = int(input("Write how many ml of water do you want to add:"))
-    remainings['water'] += add_water
+    ingredients_stock['water'] += add_water
     add_milk = int(input("Write how many ml of milk do you want to add:"))
-    remainings['milk'] += add_milk
+    ingredients_stock['milk'] += add_milk
     add_beans = int(input("Write how many grams of coffee beans "
                           "do you want to add:"))
-    remainings['beans'] += add_beans
+    ingredients_stock['beans'] += add_beans
     add_cups = int(input("Write how many cups "
                          "do you want to add:"))
-    remainings['cups'] += add_cups
+    ingredients_stock['cups'] += add_cups
 
 
-def take():
+def extract_money():
 
-    print("I gave you $", remainings['money'])
-    remainings['money'] = 0
+    print("I gave you $", ingredients_stock['money'])
+    ingredients_stock['money'] = 0
 
 
-def amount_checker(cup):
+def is_stock_enough(cup):
 
     if cup == 1:
-        if remainings['water'] < 250:
+        if ingredients_stock['water'] < 250:
             return 'water'
-        elif remainings['beans'] < 16:
+        elif ingredients_stock['beans'] < 16:
             return 'beans'
-        elif remainings['cups'] < 1:
+        elif ingredients_stock['cups'] < 1:
             return 'cups'
         else:
             return True
 
     if cup == 2:
-        if remainings['water'] < 350:
+        if ingredients_stock['water'] < 350:
             return 'water'
-        elif remainings['milk'] < 75:
+        elif ingredients_stock['milk'] < 75:
             return 'milk'
-        elif remainings['beans'] < 20:
+        elif ingredients_stock['beans'] < 20:
             return 'beans'
-        elif remainings['cups'] < 1:
+        elif ingredients_stock['cups'] < 1:
             return 'cups'
         else:
             return True
 
     if cup == 3:
-        if remainings['water'] < 200:
+        if ingredients_stock['water'] < 200:
             return 'water'
-        elif remainings['milk'] < 100:
+        elif ingredients_stock['milk'] < 100:
             return 'milk'
-        elif remainings['beans'] < 12:
+        elif ingredients_stock['beans'] < 12:
             return 'beans'
-        elif remainings['cups'] < 1:
+        elif ingredients_stock['cups'] < 1:
             return 'cups'
         else:
             return True
 
 
-def coffee_maker(cup):
+def make_coffee(cup):
 
-    status = amount_checker(cup)
+    status = is_stock_enough(cup)
     # espresso
     if cup == 1 and status is True:
         print('I have enough resources, making you a coffee!')
-        remainings['water'] -= 250
-        remainings['beans'] -= 16
-        remainings['cups'] -= 1
-        remainings['money'] += 4
+        ingredients_stock['water'] -= 250
+        ingredients_stock['beans'] -= 16
+        ingredients_stock['cups'] -= 1
+        ingredients_stock['money'] += 4
         main()
     elif cup == 1 and status is not True:
         print('Sorry, not enough ' + str(status))
@@ -116,11 +116,11 @@ def coffee_maker(cup):
     # latte
     if cup == 2 and status is True:
         print('I have enough resources, making you a coffee!')
-        remainings['water'] -= 350
-        remainings['milk'] -= 75
-        remainings['beans'] -= 20
-        remainings['cups'] -= 1
-        remainings['money'] += 7
+        ingredients_stock['water'] -= 350
+        ingredients_stock['milk'] -= 75
+        ingredients_stock['beans'] -= 20
+        ingredients_stock['cups'] -= 1
+        ingredients_stock['money'] += 7
         main()
     elif cup == 2 and status is not True:
         print('Sorry, not enough ' + str(status))
@@ -128,11 +128,11 @@ def coffee_maker(cup):
     # cappuccino
     if cup == 3 and status is True:
         print('I have enough resources, making you a coffee!')
-        remainings['water'] -= 200
-        remainings['milk'] -= 100
-        remainings['beans'] -= 12
-        remainings['cups'] -= 1
-        remainings['money'] += 6
+        ingredients_stock['water'] -= 200
+        ingredients_stock['milk'] -= 100
+        ingredients_stock['beans'] -= 12
+        ingredients_stock['cups'] -= 1
+        ingredients_stock['money'] += 6
         main()
     elif cup == 3 and status is not True:
         print('Sorry, not enough ' + str(status))
